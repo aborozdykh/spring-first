b5ce77737e94f1c071cf8649689eebb0d56e1bff
Andrii Borozdykh <aborozdykh@gmail.com>
2020-06-02 18:19:46 +0300

    Added try-catch-finally in DAO

--- src/main/java/me/aborozdykh/springfirst/dao/impl/UserDaoImpl.java
+++ src/main/java/me/aborozdykh/springfirst/dao/impl/UserDaoImpl.java
@@ -1,10 +1,14 @@
 package me.aborozdykh.springfirst.dao.impl;
 
 import java.util.List;
-import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import me.aborozdykh.springfirst.dao.UserDao;
+import me.aborozdykh.springfirst.exceptions.DataProcessingException;
 import me.aborozdykh.springfirst.models.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -16,13 +20,33 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void add(User user) {
-        sessionFactory.openSession().save(user);
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.openSession()
-                .createQuery("from User", User.class);
-        return query.getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<User> cr = cb.createQuery(User.class);
+            cr.from(User.class);
+            return session.createQuery(cr).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get all users", e);
+        }
     }
 }
