551c28081552d774687e8ff0afe867a5a5443a2a
Andrii Borozdykh <aborozdykh@gmail.com>
2020-06-04 16:37:55 +0300

    Added UserResponseDto

---
+++ src/main/java/me/aborozdykh/springfirst/config/MyWeAppInitializer.java
@@ -0,0 +1,20 @@
+package me.aborozdykh.springfirst.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWeAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/Hello123.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xeddin
+  Date: 04.06.2020
+  Time: 15:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Heool !</h1>
+</body>
+</html>
